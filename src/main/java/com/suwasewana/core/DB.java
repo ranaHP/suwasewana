6720c62d52ee681660bcd5b3c72bd5cc036d5ed8
ranaHP <2019cs135@stu.ucsc.lk>
2021-12-03 21:03:36 +0530

    This commit for user Appointment pull with dev

--- src/main/java/com/suwasewana/core/DB.java
+++ src/main/java/com/suwasewana/core/DB.java
@@ -8,7 +8,7 @@ public class DB {
 //    private final String jdbcURL = "Mysql@127.0.0.1:3306/suwasewana_db";
 
     private String jdbcUsername = "root";
-    private String jdbcPassword = "1234@Akila";
+    private String jdbcPassword = "";
 
 //    private String jdbcUsername = "root";
 //    private String jdbcPassword = "";
