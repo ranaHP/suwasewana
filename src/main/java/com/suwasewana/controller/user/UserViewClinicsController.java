2fc751f7b5563b331b7ad887061f364c12825254
Chathuranga98 <2019cs018@stu.ucsc.lk>
2021-12-04 18:44:30 +0530

    This commit for user view clinics backend part 1

---
+++ src/main/java/com/suwasewana/controller/user/UserViewClinicsController.java
@@ -0,0 +1,91 @@
+package com.suwasewana.controller.user;
+
+import com.google.gson.Gson;
+import com.suwasewana.dao.AppointmentDAO;
+import com.suwasewana.dao.PHIDAO;
+import com.suwasewana.dao.UserDAO;
+import com.suwasewana.dao.clinicalAnnouncementsDAO;
+import com.suwasewana.model.CreateClinicAnnouncementsModel;
+import com.suwasewana.model.VaccineClinicAnnouncementsModel;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/user-view-clinic-controller/*")
+public class UserViewClinicsController extends HttpServlet {
+    UserDAO userDAO;
+    clinicalAnnouncementsDAO cilinicalannouncementsDAO;
+    private Gson gson = new Gson();
+
+    public void init() {
+        cilinicalannouncementsDAO = new clinicalAnnouncementsDAO();
+        userDAO = new UserDAO();
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        doGet(req, res);
+
+    }
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        res.setCharacterEncoding("UTF-8");
+        String getUrlData [] = req.getRequestURI().split("/");
+//        res.getWriter().println(" request uri"  + req.getRequestURI() + "  data -: " +  getUrlData[getUrlData.length-1] );
+        try {
+            RequestDispatcher rd;
+            switch (getUrlData[getUrlData.length-1]) {
+                case "create":
+                    userViewclinics(req, res);
+                    break;
+//                case "view":
+//                    userViewAppointment(req, res);
+//                    break;
+                default:
+                    res.getWriter().println("404 Page not Found");
+                    break;
+            }
+        } catch (Exception error) {
+            throw new ServletException(error);
+        }
+
+//        String uNic = "";
+//        Cookie[] cookies = req.getCookies();
+//        if(cookies !=null){
+//            for(Cookie cookie : cookies){
+//                if(cookie.getName().equals("unic")) uNic = cookie.getValue();
+//            }
+//        }
+//        if(uNic == null) res.sendRedirect("login.html");
+//        res.getWriter().println(uNic);
+
+    }
+
+    private void  userViewclinics(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.getWriter().println("announcement view");
+        System.out.println("data come to controller");
+        CreateClinicAnnouncementsModel viewclinic = new CreateClinicAnnouncementsModel(
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""
+        );
+        ArrayList<CreateClinicAnnouncementsModel> result= cilinicalannouncementsDAO.ViewclinicAnnouncements(viewclinic);
+        res.getWriter().println(gson.toJson(result));
+    }
+
+}
